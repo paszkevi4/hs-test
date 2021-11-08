b97d7437ca2c5183c4a1d355b98e3d9b22d7b95e
Pavel <paszkevi4@gmail.com>
2021-11-09 01:10:16 +0200

    added gh-pages, fixed buttons-overflow bug

--- src/components/generated/index.js
+++ src/components/generated/index.js
@@ -1,6 +1,5 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import Hoc from '../../HOC'
-import { generateData } from '../../utils'
 import TreeController from '../tree'
 import { GeneartePrompt } from './generatePrompt'
 import './genearted.css'
